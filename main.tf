7d513f1e208c76405265b69f21a8348ee3049bc2
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-11 22:51:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,7 @@ module "iam_policy" {
 }
 
 module "iam_role" {
-  source     = "./modules/iam-role"
-  policy_arn = module.iam_policy.iam_policy_arn
+  source = "./modules/iam-role"  # No need to pass policy_arn anymore
 }
 
 module "service_catalog_portfolio" {
@@ -18,10 +17,9 @@ module "service_catalog_portfolio" {
 }
 
 module "service_catalog_product" {
-  source              = "./modules/service-catalog-product"
-  portfolio_id        = module.service_catalog_portfolio.portfolio_id
-  product_name        = "Simple S3 bucket1"
-  owner_name          = "Akshaya"
-  product_version     = "v1"
+  source           = "./modules/service-catalog-product"
+  portfolio_id     = module.service_catalog_portfolio.portfolio_id
+  product_name     = "Simple S3 bucket1"
+  owner_name       = "Akshaya"
+  product_version  = "v1"
 }
-
