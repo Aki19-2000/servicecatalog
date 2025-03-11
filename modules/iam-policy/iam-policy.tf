a5dde02610c0d91e2b203d6b373659edaa03a6f7
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-11 22:24:31 +0530

    Create iam-policy.tf

---
+++ modules/iam-policy/iam-policy.tf
@@ -0,0 +1,57 @@
+resource "aws_iam_policy" "s3_resource_creation_and_artifact_access_policy" {
+  name        = "S3ResourceCreationAndArtifactAccessPolicy1"
+  description = "Policy to allow access to create and manage S3 buckets and artifacts"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : "s3:GetObject",
+        "Resource" : "*",
+        "Condition" : {
+          "StringEquals" : {
+            "s3:ExistingObjectTag/servicecatalog:provisioning" : "true"
+          }
+        }
+      },
+      {
+        "Action" : [
+          "s3:CreateBucket*",
+          "s3:DeleteBucket*",
+          "s3:Get*",
+          "s3:List*",
+          "s3:PutBucketTagging"
+        ],
+        "Resource" : "arn:aws:s3:::*",
+        "Effect" : "Allow"
+      },
+      {
+        "Action" : [
+          "resource-groups:CreateGroup",
+          "resource-groups:ListGroupResources",
+          "resource-groups:DeleteGroup",
+          "resource-groups:Tag"
+        ],
+        "Resource" : "*",
+        "Effect" : "Allow"
+      },
+      {
+        "Action" : [
+          "tag:GetResources",
+          "tag:GetTagKeys",
+          "tag:GetTagValues",
+          "tag:TagResources",
+          "tag:UntagResources"
+        ],
+        "Resource" : "*",
+        "Effect" : "Allow"
+      }
+    ]
+  })
+}
+
+output "iam_policy_arn" {
+  value = aws_iam_policy.s3_resource_creation_and_artifact_access_policy.arn
+}
