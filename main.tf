043b0372fa04b4f9a1a351a1931fd58f26ece96f
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-11 22:23:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"  # Change this to your region
+  region = "us-east-1"  # Change this to your AWS region
 }
 
 module "iam_policy" {
@@ -7,15 +7,25 @@ module "iam_policy" {
 }
 
 module "iam_role" {
-  source       = "./modules/iam-role"
-  policy_arn   = module.iam_policy.iam_policy_arn
+  source     = "./modules/iam-role"
+  policy_arn = module.iam_policy.iam_policy_arn
 }
 
 module "service_catalog_portfolio" {
-  source        = "./modules/service-catalog-portfolio"
+  source         = "./modules/service-catalog-portfolio"
   portfolio_name = "S3 bucket1"
-  owner_name    = "Akshaya"
+  owner_name     = "Akshaya"
 }
 
 module "service_catalog_product" {
-  source          
+  source              = "./modules/service-catalog-product"
+  portfolio_id        = module.service_catalog_portfolio.portfolio_id
+  product_name        = "Simple S3 bucket1"
+  owner_name          = "Akshaya"
+  product_version     = "v1"
+  template_file_path  = "s3bucket.tar"
+}
+
+module "s3_bucket" {
+  source = "./modules/s3-bucket"
+}
