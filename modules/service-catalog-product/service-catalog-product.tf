5e8934993df80855fe324274cee1d427f20112b2
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 09:45:46 +0530

    Update service-catalog-product.tf

--- modules/service-catalog-product/service-catalog-product.tf
+++ modules/service-catalog-product/service-catalog-product.tf
@@ -2,17 +2,22 @@ resource "aws_servicecatalog_product" "simple_s3_bucket_product" {
   name          = var.product_name
   owner         = var.product_owner
   description   = "A simple S3 bucket product"
+  distributor   = "example-distributor"
+  support_description = "Example support description"
+  support_email       = "support@example.com"
+  support_url         = "https://example.com/support"
+  type          = "CLOUD_FORMATION_TEMPLATE"
   product_type  = "EXTERNAL"
-  provisioning_artifact {
-    name        = "v1"
-    description = "Initial version"
-    type        = "CLOUD_FORMATION_TEMPLATE"
-    template_url = "https://github.com/Aki19-2000/servicecatalog/raw/main/s3bucket.tar.gz"  # Correct raw URL
+  provisioning_artifact_parameters {
+    name           = "v1"
+    description    = "Version 1"
+    info           = {
+      "LoadTemplateFromURL" =  "https://github.com/Aki19-2000/servicecatalog/raw/main/s3bucket.tar.gz"
+    }
+    type           = "EXTERNAL"
   }
-
-  portfolio_id = var.portfolio_id
 }
-
+  
 output "product_id" {
   value = aws_servicecatalog_product.simple_s3_bucket_product.id
 }
