391a0ebe1277c1e73a1d24b52755fdcad0c29f19
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 09:51:29 +0530

    Update service-catalog-portfolio.tf

--- modules/service-catalog-portfolio/service-catalog-portfolio.tf
+++ modules/service-catalog-portfolio/service-catalog-portfolio.tf
@@ -1,7 +1,7 @@
 resource "aws_servicecatalog_portfolio" "s3_bucket_portfolio" {
-  name          = var.portfolio_name
+  name          = "s3bucket1"
   description   = "Portfolio to manage S3 bucket products"
-  provider_name = var.owner_name
+  provider_name = "akshaya"
 }
 
 output "portfolio_id" {
