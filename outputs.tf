cef9c69cfc85c546b657d4c09de100c1102483b4
Aki19-2000 <72126254+Aki19-2000@users.noreply.github.com>
2025-03-12 10:03:37 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,1 @@
-output "iam_policy_arn" {
-  value = module.iam_policy.iam_policy_arn
-}
 
-output "iam_role_arn" {
-  value = module.iam_role.iam_role_arn
-}
-
-output "portfolio_id" {
-  value = module.service_catalog_portfolio.portfolio_id
-}
-
-output "product_id" {
-  value = module.service_catalog_product.product_id
-}
